8ea1f0d639754d203bf9bcd0c2359b9c2dd9fb12
okaydokay97 <rubens.fernandes97@gmail.com>
2020-05-18 20:16:30 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,29 @@
+require 'pry'
 class Gym
   attr_reader :name
-
+  attr_accessor
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all 
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def members_list
+    list_of_members = memberships.map {|membership| membership.lifter.name}
+    list_of_members
+  end
+
+  def all_lift_total
+      all_members = memberships.map{|membership| membership.lifter.lift_total}
+      all_members.sum
+  end
+
 end
